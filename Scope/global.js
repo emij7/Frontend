983e3b84a30caf07a681e79ef97e946748f45008
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-02-21 20:49:24 -0300

    agregando carpetas

---
+++ Scope/global.js
@@ -0,0 +1,13 @@
+var hello = 'Hello'
+let world ='world'
+const helloWorld = 'HELLO WORLD!!!'
+console.log(hello)
+//variables globales
+
+const aFunction = () =>{
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+aFunction()
