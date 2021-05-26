f7e9368dab17b53f5d7493e9bc2a7b49760b5750
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-05-26 20:43:42 -0300

    Deleting unnecessary course.

--- Fundamentos JS/Probando promesas/promesa.js
+++ Fundamentos JS/Probando promesas/promesa.js
@@ -2,18 +2,34 @@ let numero = 7;
 let promesa = (x) => {
     return new Promise(function(res,rej){
         if(x==7){
-            res()
+            res('era un 7')     //Si se cumple la promesa res va a devolver lo que tenga dentro de los ()
         }else{
-        rej(x)
+        rej('no era un 7 bro')
         }
     }
 
     )}
 
 promesa(numero)
-.then(x=>{
-    console.log('Era un 7')
+.then(success=>{
+    console.log('Success ' + success)   //Y acá llamamos con success lo que devolvió el resolve de la promesa
+    console.log(numero)
 })
-.catch(x=>{
-    console.log('No era un 7 bro')
+.catch(error=>{
+    console.log('Error '+ error)
 })
+
+// const makeServerRequest = new Promise((resolve, reject) => {
+//     // responseFromServer is set to true to represent a successful response from a server
+//     let responseFromServer = true;
+      
+//     if(responseFromServer) {
+//       resolve("We got the data");
+//     } else {  
+//       reject("Data not received");
+//     }
+//   });
+  
+//   makeServerRequest.then(result=>{
+//     console.log(result)
+//   })
