ef495c42d01b4317df396170828039d2e12524c5
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-08-30 18:58:08 -0300

    Fixed repeated numbers on binary search trees insert method.

--- Curso estructura de datos/trees/tree.js
+++ Curso estructura de datos/trees/tree.js
@@ -19,21 +19,54 @@ class BSTree {
             let condition = true
             if(value == currentNode.value){
                 console.log('error, valor repetido')
+            }else{
+                while(condition == true){
+                    value == currentNode.value?(
+                        console.log('error, valor repetido'),
+                        condition = false)    
+                        :value<currentNode.value?
+                            currentNode.left == null?(
+                                currentNode.left=newValue,
+                                condition = false)
+                                :currentNode = currentNode.left
+                            :currentNode.right == null?(
+                                currentNode.right=newValue,
+                                condition = false
+                            )
+                                :currentNode = currentNode.right                    
+                }
             }
-            while(condition == true){
-                value<currentNode.value?
-                    currentNode.left == null?(
-                        currentNode.left=newValue,
-                        condition = false)
-                        :currentNode = currentNode.left
-                    :currentNode.right == null?(
-                        currentNode.right=newValue,
-                        condition = false
-                    )
-                        :currentNode = currentNode.right
-            }       
+                   
         }
         return this
+    }
+    find(value, current){
+        if(value==current.value){
+            return (true,
+                console.log('Valor existente'))
+        }else{
+            value<current.value?
+                current.left == null?
+                    console.log('Valor inexistente')
+                    :(  
+                        current = current.left,
+                        this.find(value,current)
+                        )
+                :current.right == null?
+                    console.log('Valor inexistente')
+                    :(  
+                        current = current.right,
+                        this.find(value,current)
+                        )                
+        }
+    }
+    search(value, current = this.root){
+        if(this.root == null){
+            return console.log('Tree vacío.')
+        }else{
+            this.find(value, current)
+        }
+        
     }
 }
 let tree = new BSTree()
